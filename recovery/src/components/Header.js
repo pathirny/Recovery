efba2be56da0a02ee21c68fd16403be37d8d8d90
Patryk Hirny <pathirny@googlemail.com>
2024-02-20 17:54:32 +0000

    added link tag to home button

--- recovery/src/components/Header.js
+++ recovery/src/components/Header.js
@@ -5,7 +5,9 @@ import Logo from "../recoveryLogo.png";
 export default function Header() {
   return (
     <div className="header">
+      <a href="/">
       <FontAwesomeIcon icon={faHouse} size="2xl" style={{ color: "#ffff" }} />
+      </a>
       <div>
         <img src={Logo} alt="Logo" className="logoImage"></img>
       </div>
