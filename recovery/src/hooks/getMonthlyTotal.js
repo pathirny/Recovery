18fbacfa2946ec125776fa374c350ac604e53e24
Patryk Hirny <pathirny@googlemail.com>
2024-02-29 18:53:05 +0000

    added variables for all costs

--- recovery/src/hooks/getMonthlyTotal.js
+++ recovery/src/hooks/getMonthlyTotal.js
@@ -20,9 +20,24 @@ const useMonthlyTotal = (firstDay, lastDay) => {
 
         let res = data;
         let sumTotal = 0;
+        let sumPetrol = 0;
+        let sumTyres = 0;
+        let sumRepairs = 0;
+        let sumIncome = 0;
+        let sumOtherCosts = 0;
         for (const key in res) {
           let elementTotal = res[key].total;
+          let elementPetrol = res[key].petrol;
+          let elementRepairs = res[key].repairs;
+          let elementIncome = res[key].income;
+          let elementOtherCosts = res[key].other_costs;
+          let elementTyres = res[key].tyres;
           sumTotal += elementTotal;
+          sumPetrol += elementPetrol;
+          sumTyres += sumTyres;
+          sumRepairs += elementRepairs;
+          sumIncome += elementIncome;
+          sumOtherCosts += elementOtherCosts;
         }
         setMonthlyTotal(sumTotal);
         console.log(res)
